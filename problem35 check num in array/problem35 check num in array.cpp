2ab8c55088669259e4f855d76d9a3fb3f48bc461
gomaa <gogochemist35@gmail.com>
2023-09-20 02:51:00 +0300

    Add project files.

---
+++ problem35 check num in array/problem35 check num in array.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+
+
+
+int RandomNumber(int From, int To)
+{
+	return rand() % (To - From + 1) + From;
+}
+void readArray(int arr[], int& length)
+{
+	cout << "How Many elements in the array?\n";
+	cin >> length;
+
+	for (int i = 0; i < length; i++)
+	{
+		arr[i] = RandomNumber(1, 100);
+	}
+}
+void printArray(int arr[], int length)
+{
+	cout << "Array 1 Elements: \n";
+	for (int i = 0; i < length; i++)
+	{
+		cout << arr[i] << " ";
+	}
+	cout << "\n";
+}
+
+int readNumberToCheck()
+{
+	int num;
+	cout << "Please enter a number to search for?\n";
+	cin >> num;
+
+	return num;
+}
+
+int NumberPositionInArray(int arr[], int length, int num)
+{
+	for (int i = 0; i < length; i++)
+	{
+		if (num == arr[i])
+			return i;
+	}
+	return -1;
+}
+
+bool checkNumberInArray(int arr[], int length, int num)
+{
+	if (NumberPositionInArray(arr, length, num) == -1)
+		return false;
+	else
+		return true;
+}
+
+void printFindNumberCheck(int arr[], int length, int num)
+{
+	if (checkNumberInArray(arr, length, num))
+	{
+		int numPosition = NumberPositionInArray(arr, length, num);
+		cout << "Number Yor are looking for is : " << num << endl;
+		cout << "Yes, the number is found :-) " << endl;
+	}
+	else
+	{
+		cout << "Number Yor are looking for is : " << num << endl;
+		cout << "No, the number is not found :-( " << endl;
+	}
+}
+
+
+int main()
+{
+	srand((unsigned)time(NULL));
+
+	int arr[100], length;
+
+	readArray(arr, length);
+	printArray(arr, length);
+	int numToCheck = readNumberToCheck();
+
+	printFindNumberCheck(arr, length, numToCheck);
+
+
+	return main();
+}
